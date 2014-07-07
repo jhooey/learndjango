26acad680debfbf7006aaea41bb9abb3f4b74db4
Jacob Hooey <jhooey@gmail.com>
2014-07-07 12:07:40 -0700

    / root - Points to the created home_page

--- learndjango/urls.py
+++ learndjango/urls.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'learndjango.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
